afb2049ac567a41cd66e5be10699504db7f8d4ef
Ayushi Jindal (ayujinda) <ayujinda@cisco.com>
2024-02-29 11:47:03 +0530

    My Maven Project version1

---
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -0,0 +1,14 @@
+package com.cisco.simplemavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the world of Jenkins" );
+        System.out.println("Hello World from my Simple Project");
+    }
+}
