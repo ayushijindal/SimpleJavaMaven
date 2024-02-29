a926b299c7a84c602f32e3c3eff7a53f31996959
Ayushi Jindal (ayujinda) <ayujinda@cisco.com>
2024-02-29 15:13:23 +0530

    made changes removed duplicate line

--- src/main/java/com/cisco/simplemavenproject/App.java
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {	
-    	HelloWorld.print();
+    	
     	HelloWorld.print();
         System.out.println( "Welcome to the world of Jenkins" );
         System.out.println("Hello World from my Simple Project");
