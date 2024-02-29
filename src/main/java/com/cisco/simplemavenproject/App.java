f5c0e1d0abc4619cb5fa9f901988b5d39f00370e
Ayushi Jindal (ayujinda) <ayujinda@cisco.com>
2024-02-29 15:15:04 +0530

    removed empty lines code

--- src/main/java/com/cisco/simplemavenproject/App.java
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -7,8 +7,7 @@ package com.cisco.simplemavenproject;
 public class App 
 {
     public static void main( String[] args )
-    {	
-    	
+    {
     	HelloWorld.print();
         System.out.println( "Welcome to the world of Jenkins" );
         System.out.println("Hello World from my Simple Project");
